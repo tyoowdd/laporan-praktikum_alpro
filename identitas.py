647947a7a9bd9c44b6510f84c424673d71eaaf65
tyoowdd <tyoowdd@gmail.com>
2026-08-23 15:34:51 +0700

    Add files via upload

---
+++ identitas.py
@@ -0,0 +1,5 @@
+print("\n NAMA : NUGROHO PRASETYO WIDODO");
+print("\n NIM  : 2605060036");
+print("\n TTL  : GROBOGAN, 25 FEBRUARI 2008");
+print("\n JK   : LAKI-LAKI");
+print("\n HOBI : BERNYANYI");
