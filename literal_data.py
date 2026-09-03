c95f8ac1cdce49dfa7a24c75eb50de62d1ec1ebd
tyoowdd <tyoowdd@gmail.com>
2026-09-03 22:04:04 +0700

    Add files via upload

---
+++ literal_data.py
@@ -0,0 +1,26 @@
+# tugas 1
+nama = 'nugroho prasetyo widodo'
+umur = 18
+berat = 57.4
+
+print('Nama: ' + nama)
+print('Umur: ', int(umur))
+print('Berat: ', float(berat))
+
+#tugas 2
+angka_string = "123"
+angka_float = 45.67
+angka_integer = 89
+
+print('Angka String: ', int(angka_string)) # mengubah string menjadi integer
+print('Angka Float: ', int(angka_float)) # mengubah float menjadi integer
+print('Angka Integer: ', float(angka_integer)) # mengubah integer menjadi float
+print('Angka Integer: ', str(angka_integer)) # mengubah integer menjadi string
+
+#tugas 3
+usia = int(input('Masukkan usia: ')) 
+tb = float(input('Masukkan tinggi badan: '))
+nama = str(input('Masukkan nama: '))
+print("Usia: ", usia)
+print("Tinggi Badan: ", tb)
+print("Nama: ", nama)
