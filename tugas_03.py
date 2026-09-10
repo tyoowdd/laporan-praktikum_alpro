595fa5b7d49f76a06df9ecc173388d0d128c612f
tyoowdd <tyoowdd@gmail.com>
2026-09-10 10:46:34 +0700

    Add files via upload

---
+++ tugas_03.py
@@ -0,0 +1,25 @@
+# tugas 03
+
+panjang = 12
+lebar = 5
+tinggi = 8
+
+# soal 1 a
+luas = 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi) # rumus luas balok
+print("luas permukaan =",luas)
+volume = panjang * lebar * tinggi # rumus volume balok
+print("volume =",volume)
+keliling = 4 * (panjang + lebar + tinggi) # rumus keliling balok
+print("keliling =",keliling)
+
+# soal 1 b
+hasil = luas > 50
+print('apakah luas > 50','=', hasil)
+
+# soal 1 c
+hasil = volume == 480
+print('apakah volume tersebut bernilai 480','=',hasil)
+
+
+
+
